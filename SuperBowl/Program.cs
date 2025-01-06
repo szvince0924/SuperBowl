59f3b23cca5fbd1f1ae391203312ad007c141668
szvince0924 <163414915+szvince0924@users.noreply.github.com>
2025-01-06 14:03:22 +0100

    4

--- SuperBowl/Program.cs
+++ SuperBowl/Program.cs
@@ -4,12 +4,23 @@
     {
         static void Main(string[] args)
         {
-            List<Donto> Dontok = new();
+            List<Döntő> Dontok = new();
             foreach (var sor in File.ReadAllLines("SuperBowl.txt").Skip(1))
             {
-                Dontok.Add(new Donto(sor));
+                Dontok.Add(new Döntő(sor));
             }
-            Console.WriteLine($"4.feladat:dontok szama")
+            Console.WriteLine($"4.feladat:dontok szama: {Dontok.Count}");
+
+            int pontkölömbség = 0;
+            foreach (var d in Dontok)
+            {
+               string[] e = d.Eredmény.Split('-');
+
+                pontkölömbség += Math.Abs(int.Parse(e[0]) - int.Parse(e[1]);
+
+            }
+            double átlag = pontkölömbség / Dontok.Count;
+            Console.WriteLine($"5.feladat: Átlagos pontkülömbség: {átlag:0.0}");
         }
     }
 }
