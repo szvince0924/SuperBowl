bee28dc75b62c418f6dfd7a8693fa2116de43f25
szvince0924 <163414915+szvince0924@users.noreply.github.com>
2025-01-06 14:12:40 +0100

    5

--- SuperBowl/Program.cs
+++ SuperBowl/Program.cs
@@ -16,11 +16,14 @@
             {
                string[] e = d.Eredmény.Split('-');
 
-                pontkölömbség += Math.Abs(int.Parse(e[0]) - int.Parse(e[1]);
+                pontkölömbség += Math.Abs(int.Parse(e[0])) - int.Parse(e[1]);
 
             }
             double átlag = pontkölömbség / Dontok.Count;
             Console.WriteLine($"5.feladat: Átlagos pontkülömbség: {átlag:0.0}");
+
+            RomaiSorszam r = new("MMXXV");
+            Console.WriteLine(r.ArabSsz);
         }
     }
 }
